cd8277728c903e50758b1ec0eaf4ae62a8f04d4a
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-10-08 17:29:30 +0200

    Public T Hent(int indeks) metode - Fullført

--- src/com/oblig2/DobbeltLenketListe.java
+++ src/com/oblig2/DobbeltLenketListe.java
@@ -148,7 +148,8 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public T hent(int indeks) {
-        throw new NotImplementedException();
+        indeksKontroll(indeks, false);
+        return finnNode(indeks).verdi;
     }
 
     @Override
