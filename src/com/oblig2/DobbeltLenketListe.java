c7929fc0ccdc88193247d01feccc2242762a8df4
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-10-08 16:43:48 +0200

    Begynner på oppgave 2

--- src/com/oblig2/DobbeltLenketListe.java
+++ src/com/oblig2/DobbeltLenketListe.java
@@ -7,6 +7,7 @@ package com.oblig2;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.sql.SQLOutput;
 import java.util.Comparator;
 import java.util.ConcurrentModificationException;
 import java.util.NoSuchElementException;
@@ -20,6 +21,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     /**
      * Node class
+     *
      * @param <T>
      */
 
@@ -44,13 +46,42 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     private int antall;            // antall noder i listen
     private int endringer;         // antall endringer i listen
 
+
     public DobbeltLenketListe() {
-     //   throw new NotImplementedException();
+        //   throw new NotImplementedException();
     }
 
+
     public DobbeltLenketListe(T[] a) {
-        
-    }
+        DobbeltLenketListe liste = new DobbeltLenketListe();
+
+        liste.hode = new DobbeltLenketListe.Node(a);
+        liste.hale = liste.hode;
+
+        DobbeltLenketListe.Node p = liste.hale;
+        p.verdi = a[0];
+        for (int i = 1; i < a.length; i++) {
+            DobbeltLenketListe.Node q = new DobbeltLenketListe.Node(a[i]);
+            q.forrige = p;
+            p.neste = q;
+
+            p = q;
+
+            liste.hale = q;
+        }
+
+            if(a.length < 1) {
+                throw new NullPointerException("Tabellen a er null!");
+            }
+            if(a.length == 1) {
+                hode = hale;
+            }
+            if(a == null){
+                String str = null;
+                str = Objects.requireNonNull(str);
+            }
+        }
+
 
     public Liste<T> subliste(int fra, int til) {
         throw new NotImplementedException();
@@ -74,7 +105,12 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public boolean leggInn(T verdi) {
-        throw new NotImplementedException();
+        if(verdi == null) {
+            String str = null;
+            str =  Objects.requireNonNull(str);
+        }
+
+        return true;
     }
 
     @Override
@@ -170,3 +206,18 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         throw new NotImplementedException();
     }
 } // class DobbeltLenketListe
+
+/*
+ for (int i = 0; i < a.length && a[i] == null; i++) {
+            if (i < a.length) {
+                Node<T> p = hode = new Node<>(a[i]);
+                antall = 1;
+                for (i++; i < a.length; i++) {
+                    if (a[i] != null) {
+                        p = p.neste = new Node<>(a[i]);
+                        antall++;
+                    }
+                }
+                hale = p;
+            }
+ */
