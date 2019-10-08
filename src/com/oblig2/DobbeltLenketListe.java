3783f52cf2d2e51de64e2befe7147044f5543b78
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-10-08 17:24:31 +0200

    finnNode metode - Fullført (3.3.3 a)

--- src/com/oblig2/DobbeltLenketListe.java
+++ src/com/oblig2/DobbeltLenketListe.java
@@ -51,6 +51,23 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         //   throw new NotImplementedException();
     }
 
+    private Node<T> finnNode(int indeks) {
+        if (indeks < (antall / 2)) {
+            Node<T> p = hale;
+            for (int i = 0; i < indeks; i++) {
+                p = p.forrige;
+            }
+            return p;
+        }
+        else {
+            Node<T> p = hode;
+            for (int i = 0; i < indeks; i++) {
+                p = p.neste;
+            }
+            return p;
+        }
+    }
+
 
     public DobbeltLenketListe(T[] a) {
         DobbeltLenketListe liste = new DobbeltLenketListe();
