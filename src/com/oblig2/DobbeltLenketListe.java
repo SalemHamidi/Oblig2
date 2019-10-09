cfb502e532b1d5da2127ef6c0bdfb153571e7a92
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-10-09 14:26:27 +0200

    Oppgave 8b - Fullført

--- src/com/oblig2/DobbeltLenketListe.java
+++ src/com/oblig2/DobbeltLenketListe.java
@@ -320,7 +320,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public Iterator<T> iterator() {
-        throw new NotImplementedException();
+        return new DobbeltLenketListeIterator();
     }
 
     public Iterator<T> iterator(int indeks) {
